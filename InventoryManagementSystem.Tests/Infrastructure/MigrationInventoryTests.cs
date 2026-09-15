325413bf4f740a229fe1f58a1e50037957414fed
M.F.M Fazrin <18024930+nirzaf@users.noreply.github.com>
2026-09-15 08:50:34 +0300

    test(data): protect initial EF migration
    Closes #6

---
+++ InventoryManagementSystem.Tests/Infrastructure/MigrationInventoryTests.cs
@@ -0,0 +1,24 @@
+using FluentAssertions;
+using InventoryManagementSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InventoryManagementSystem.Tests.Infrastructure;
+
+public class MigrationInventoryTests
+{
+    [Fact]
+    public void Infrastructure_contains_initial_migration_and_model_snapshot()
+    {
+        var infrastructureAssembly = typeof(InventoryDbContext).Assembly;
+        var migrationTypes = infrastructureAssembly.GetTypes()
+            .Where(type => typeof(Migration).IsAssignableFrom(type))
+            .Select(type => type.Name)
+            .ToArray();
+
+        migrationTypes.Should().Contain("InitialCreate");
+        infrastructureAssembly.GetType(
+                "InventoryManagementSystem.Infrastructure.Migrations.InventoryDbContextModelSnapshot")
+            .Should()
+            .NotBeNull();
+    }
+}
